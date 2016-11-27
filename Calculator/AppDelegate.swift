fb1caca3292a14a86a6cafb6625ea1eae874fd06
Peter Vorwieger <peter.vorwieger@me.com>
2016-11-27 14:12:02 +0100

    Initial Commit

---
+++ Calculator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Calculator
+//
+//  Created by Peter Vorwieger on 27.11.16.
+//  Copyright © 2016 Peter Vorwieger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
